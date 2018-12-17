a8a95d6741050026d1196140e816cc7234002e17
朱骏梁 <770710869@qq.com>
2018-12-17 15:05:42 +0800

    时间动起来

--- src/components/index/index.vue
+++ src/components/index/index.vue
@@ -6,10 +6,10 @@
 				<div class="logo_img">
 					<span>山西省国资监管大数据平台</span>
 					<div class="logo_time">
-						<div class="fl time_sec">12:54:36</div>
+						<div class="fl time_sec">{{getTime}}</div>
 						<div class="fl ml20 time_week">
-							<p>星期二</p>
-							<p>2018.12.5</p>
+							<p>{{getweek}}</p>
+							<p>{{getMouth}}</p>
 						</div>
 					</div>
 				</div>
@@ -40,11 +40,12 @@
 <script>
 
 import cycleFour from "./module/four/four";
+import moment from "moment";
 import mapAndBar from "./module/mapAndBar/mapAndBar";
 import RightInfo from "./module/company/RightInfo";
 import windAndCom from "./module/windAndCom/windAndCom";
 import internetCompany from "./module/internetCompany/internetCompany";
-
+moment.locale('zh-cn');
 export default {
     name:"index",
     components:{
@@ -56,15 +57,22 @@ export default {
 	},
     data () {
 		return {
-			
+			getTime:moment().format('hh:mm:ss'),
+			getMouth:moment().format('YYYY.MM.DD'),
+			getweek:moment().format('dddd'),
+
 			
 		}
 	},        
 	mounted(){            
-		
+		this.changeTime();
 	},       
 	methods: {            
-		handleSubmit() {
+		changeTime() {
+			var _this = this;
+			setInterval(function(){
+				_this.getTime = moment().format('hh:mm:ss');
+			},1000)
 			
 		}
 			
--- src/components/index/module/mapAndBar/map.vue
+++ src/components/index/module/mapAndBar/map.vue
@@ -1,41 +1,103 @@
 <template>
-    <div class="map">
-        <div class="clearfix one_top">
-            <div class="fl img clearfix">
-                <span class="imgShow fl"></span><span class="ml10 fl">国资监管平台</span>
-            </div>
-            <div class="fr years " style="cursor:pointer">
-                详细列表
-            </div>
-        </div>
-        <div class="mapShow">
-            <div class="mapShow_left">
-                <div class="left_info">
-                    <p class="p">168</p>
-                    <p class="p">企业总数量</p>
-                </div>
-            </div>
-        </div>
+  <div class="map">
+    <div class="clearfix one_top">
+      <div class="fl img clearfix">
+        <span class="imgShow fl"></span>
+        <span class="ml10 fl">国资监管平台</span>
+      </div>
+      <div class="fr years" style="cursor:pointer">详细列表</div>
     </div>
+    <div class="mapShow">
+      <div class="mapShow_left">
+        <div class="left_info">
+          <p class="p">168</p>
+          <p class="p">企业总数量</p>
+        </div>
+        <div class="left_info_gray">
+          <p class="p">36</p>
+          <p class="p">一级企业</p>
+        </div>
+        <div class="left_info_gray">
+          <p class="p">132</p>
+          <p class="p">二级企业</p>
+        </div>
+      </div>
+      <div ref="mapShow" class="mapHeight"></div>
+    </div>
+  </div>
 </template>
-<script>   
-    export default {
-    name:"four",     
-    components:{
-		
-	}, 
-    data () {
-            return {               
-                
+<script>
+import echarts from "echarts";
+export default {
+  name: "mapShow",
+  components: {},
+  data() {
+    return {};
+  },
+  mounted() {
+      this.getMap();
+  },
+  methods: {
+    getMap: function() {
+      echarts.registerMap("HK", require('./mapJson/geoJson.json'));
+
+      echarts.init(this.$refs.mapShow).setOption(
+            {        
+          tooltip: {
+            trigger: "item",
+            formatter: "{b}<br/>{c}"
+          },
+
+          series: [
+            {
+              name: "香港18区人口密度",
+              type: "map",
+              mapType: "HK", // 自定义扩展图表类型3
+              roam:true,
+              aspectScale:'1.5',
+              zoom:1.2,
+               label:{   
+                  show:true
+              },
+            //   left:20,
+            //   top:20,
+            //   left:20,
+            //   left:20,
+            // color:"#fff",
+              itemStyle:{
+                    // normal: {
+                areaColor: "#366AA6",
+                borderColor: '#2D5D89',
+                color:"#fff"
+                    // }                    
+                },
+                emphasis:{
+                    itemStyle:{
+                        areaColor: "#00a8ff",
+                        color:"#fff"
+                    }                   
+
+                },
+              data: [
+                { name: "大同市", value: 20057.34 } ,               
+                { name: "太原市", value: 20057.34 }  ,              
+                { name: "朔州市", value: 20057.34 }  ,              
+                { name: "忻州市", value: 20057.34 }  ,              
+                { name: "阳泉市", value: 20057.34 }  ,              
+                { name: "吕梁市", value: 20057.34 }  ,              
+                { name: "晋中市", value: 20057.34 }  ,              
+                { name: "晋城市", value: 20057.34 }  ,              
+                { name: "临汾市", value: 20057.34 }  ,              
+                { name: "运城市", value: 20057.34 }                
+              ]
+              
             }
-        },        
-        mounted(){            
-        },       
-        methods: {  
-                
+          ]
         }
-}
+      );
+    }
+  }
+};
 </script>
 <style lang="less">
-    
 </style>
